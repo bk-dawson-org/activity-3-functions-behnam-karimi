79f6acfde5598bf6b9337968160f3359abb17ca5
behnam-karimi <61291690+behnam-karimi@users.noreply.github.com>
2024-09-12 21:07:43 +0000

    test

--- cake.py
+++ cake.py
@@ -1,9 +1,16 @@
 import math
 
 def getCylinderVolume(radius, height) :
-    volume = math.pi * (radius ** 2) * height     
+    '''
+        This function calculates the volume of a cylinder
+        param radius: This is the radius of the cylinder
+        param height: This is the height of the cylinder
+        return : The function returns the volume of a cylinder
+    '''
+    volume = math.pi * (radius ** 3) * height     
     return volume 
 
+# Claculates the volume of a cylinder with values 10 and 12
 x = getCylinderVolume(10, 12)
 y = getCylinderVolume(2, 6)
 print(round(x,4))
@@ -15,5 +22,6 @@ def getNumberOfSlices(radius, height, volumeOfSlice) :
     numberOfSlices = volume / volumeOfSlice
     return int(numberOfSlices)
 
-numberOfSlices1 = getNumberOfSlices(10, 10, 100)
+# Calculating the number of slices 
+#numberOfSlices1 = getNumberOfSlices(10, 10, 100)
 print(numberOfSlices1)
